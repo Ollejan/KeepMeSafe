6e427613456cd0e03aa10207a1c49f9accf929c0
Jonas Dahlström <jonas.dahlstrom1@gmail.com>
2016-02-11 20:13:54 +0100

    Added big button and a snackbar message.

--- mobile/src/main/java/se/mah/ad0025/keepmesafe/MainFragment.java
+++ mobile/src/main/java/se/mah/ad0025/keepmesafe/MainFragment.java
@@ -2,10 +2,12 @@ package se.mah.ad0025.keepmesafe;
 
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -24,6 +26,14 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+        Button btn_help = (Button)view.findViewById(R.id.btn_help);
+        btn_help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Messages have been sent", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
+
         return view;
     }
 
