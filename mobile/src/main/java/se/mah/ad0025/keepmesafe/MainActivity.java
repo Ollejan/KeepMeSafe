5a4a2e7b20d9da44650f1ae3e056d228157b80fd
Sebastian Aspegren <nethakaaru@gmail.com>
2016-02-14 16:58:49 +0100

    Rotation handling & permission prompt

--- mobile/src/main/java/se/mah/ad0025/keepmesafe/MainActivity.java
+++ mobile/src/main/java/se/mah/ad0025/keepmesafe/MainActivity.java
@@ -1,15 +1,21 @@
 package se.mah.ad0025.keepmesafe;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -21,6 +27,7 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, AddContactFragment.OnImportClickedListener {
 
     static final int PICK_CONTACT = 123;
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 64;
     AddContactFragment addContactFragment;
 
     @Override
@@ -40,14 +47,13 @@ public class MainActivity extends AppCompatActivity
             toggle.syncState();
             NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
             navigationView.setNavigationItemSelectedListener(this);
-            addContactFragment = new AddContactFragment();
             if (savedInstanceState != null) {
 //Här läggs det som bara ska ske vid rotation men inte första gången. Tex hämta värden via savedInstanceState.
-
+                FragmentManager fm = getSupportFragmentManager();
+                addContactFragment = (AddContactFragment) fm.findFragmentByTag("contacts");
                 return;
             }
 //Här läggs det som ska ske första gången men inte efter rotation.
-
             FragmentManager fm = getSupportFragmentManager();
             fm.beginTransaction().replace(R.id.container, new MainFragment()).commit();
         }
@@ -96,7 +102,10 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_Add) {
-            fm.beginTransaction().replace(R.id.container, addContactFragment).commit();
+
+            if(addContactFragment == null)
+            addContactFragment = new AddContactFragment();
+            fm.beginTransaction().replace(R.id.container, addContactFragment,"contacts").commit();
         } else if (id == R.id.nav_Delete) {
 
         } else if (id == R.id.nav_Edit) {
@@ -113,11 +122,90 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    openContacts();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
     public void onImportBtnClicked() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == -1) {
+// Here, thisActivity is the current activity
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_CONTACTS)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_CONTACTS)) {
+
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage("You need this permission to pick a contact and automatically import its name and number. Are you sure you don't want it?").setPositiveButton("Yes", dialogClickListener)
+                            .setNegativeButton("No", dialogClickListener).show();
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+                   requestPermission();
+
+                }
+            } else {
+               openContacts();
+            }
+        } else {
+            openContacts();
+        }
+    }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_CONTACTS},
+                MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+    }
+
+    private void openContacts(){
         Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
         intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
         startActivityForResult(intent, PICK_CONTACT);
     }
+
+    DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which){
+                case DialogInterface.BUTTON_POSITIVE:
+                  requestPermission();
+                    break;
+
+                case DialogInterface.BUTTON_NEGATIVE:
+                    //No button clicked
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
